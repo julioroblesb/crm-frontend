dacf6d85288f06f3984269f7bd98897dcda0b55b
julioroblesb <julioroblesb13@gmail.com>
2025-08-01 12:19:32 -0500

    Update GoogleSheetsSetup.jsx

--- src/components/GoogleSheetsSetup.jsx
+++ src/components/GoogleSheetsSetup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import apiService from '../lib/api';
+import apiService from '../services/api';
 
 function GoogleSheetsSetup() {
   const [sheetId, setSheetId] = useState('');
